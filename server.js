6241537bb65a306fc7fdf5ebf17feb9b90a6169c
GracefulRoseThorn <46552746+GracefulRoseThorn@users.noreply.github.com>
2019-03-14 11:00:57 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.locals._ = _;
 
 app.get('/', function (req, res) {
 
-    request.get('http://api.openweathermap.org/data/2.5/forecast?q=Lebanon,or&units=imperial&appid=8ba9df0e58c48ceb3271a1edd37e94a0', (resp) => {
+    request.get('http://api.openweathermap.org/data/2.5/forecast?q=Lebanon,usa&units=imperial&appid=8ba9df0e58c48ceb3271a1edd37e94a0', (resp) => {
         let data = '';
 
         // A chunk of data has been recieved.
